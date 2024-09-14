cd509d4427457eaa7b547b71f1b2dc9e99598816
gdziewon <gdziewon@fear.fearsome>
2024-09-14 03:15:34 +0200

    Initial commit
    Implemented a fundation for a simple shell

---
+++ y.tab.h
@@ -0,0 +1,15 @@
+#ifndef _yy_defines_h_
+#define _yy_defines_h_
+
+#define NEWLINE 257
+#define OUT_REDIRECT 258
+#define IN_REDIRECT 259
+#define APPEND_REDIRECT 260
+#define FD_OUT_REDIRECT 261
+#define FD_APPEND_REDIRECT 262
+#define PIPE 263
+#define BACKGROUND 264
+#define WORD 265
+#define INVALID 266
+
+#endif /* _yy_defines_h_ */
